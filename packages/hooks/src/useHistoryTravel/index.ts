786c5379c2f54091ab831252c06108c7928808ef
Gopal <1172597655@qq.com>
2022-08-01 18:08:05 +0800

    docs: useHistoryTravel

--- packages/hooks/src/useHistoryTravel/index.ts
+++ packages/hooks/src/useHistoryTravel/index.ts
@@ -2,14 +2,17 @@ import { useRef, useState } from 'react';
 import useMemoizedFn from '../useMemoizedFn';
 import { isNumber } from '../utils';
 
+// 定义数据类型。其中 past 和 future 维护一个队列。
 interface IData<T> {
   present?: T;
   past: T[];
   future: T[];
 }
 
+// 获取 current 值的下标
 const dumpIndex = <T>(step: number, arr: T[]) => {
   let index =
+  // 当值大于 0 的时候，前进
     step > 0
       ? step - 1 // move forward
       : arr.length + step; // move backward
@@ -22,8 +25,11 @@ const dumpIndex = <T>(step: number, arr: T[]) => {
   return index;
 };
 
-// 根据第几步，分成当前状态、之前、未来的状态
+// 将传入 targetArr，根据 step，分成当前状态、之前、未来的状态
+// 比如 2，[1,2,3,4] { _current: 2, _before: [1], _after: [3,4] }
+// 比如 -1，[1,2,3,4] { _current: 4, _before: [1, 2, 3], _after: [] }
 const split = <T>(step: number, targetArr: T[]) => {
+  // 获取 current 值的下标
   const index = dumpIndex(step, targetArr);
   return {
     _current: targetArr[index],
@@ -48,6 +54,7 @@ export default function useHistoryTravel<T>(initialValue?: T) {
 
   // 重置
   const reset = (...params: any[]) => {
+    // 重置到初始值，或提供一个新的初始值
     const _initial = params.length > 0 ? params[0] : initialValueRef.current;
     initialValueRef.current = _initial;
 
@@ -62,16 +69,19 @@ export default function useHistoryTravel<T>(initialValue?: T) {
   const updateValue = (val: T) => {
     setHistory({
       present: val,
+      // future 直接置空
       future: [],
+      // 之前的 past 和 present 都将称为 past
       past: [...past, present],
     });
   };
 
-  // 前进
+  // 前进，默认前进一步
   const _forward = (step: number = 1) => {
     if (future.length === 0) {
       return;
     }
+    // 前进则第二个参数传递的是 feature
     const { _before, _current, _after } = split(step, future);
     setHistory({
       // 旧状态，加上现在以及刚过去的
@@ -82,12 +92,13 @@ export default function useHistoryTravel<T>(initialValue?: T) {
     });
   };
 
-  // 后退
+  // 后退，默认后退一步
   const _backward = (step: number = -1) => {
     if (past.length === 0) {
       return;
     }
 
+    // 后退则第二个参数传递的是 past
     const { _before, _current, _after } = split(step, past);
     setHistory({
       past: _before,
@@ -96,7 +107,7 @@ export default function useHistoryTravel<T>(initialValue?: T) {
     });
   };
 
-  // 跳到第几步
+  // 跳到第几步，最终调用 _forward 和 _backward
   const go = (step: number) => {
     const stepNum = isNumber(step) ? step : Number(step);
     if (stepNum === 0) {
