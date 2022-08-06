cea7c228c9c6fff73f6ecf42067fb82d2fe298e6
Gopal <1172597655@qq.com>
2022-08-06 13:52:30 +0800

    docs: dev

--- packages/hooks/src/useTrackedEffect/index.ts
+++ packages/hooks/src/useTrackedEffect/index.ts
@@ -9,8 +9,11 @@ type Effect = (
 
 const diffTwoDeps = (deps1?: DependencyList, deps2?: DependencyList) => {
   //Let's do a reference equality check on 2 dependency list.
+  // 让我们对2个依赖项列表做一个引用相等性检查。
   //If deps1 is defined, we iterate over deps1 and do comparison on each element with equivalent element from deps2
+  // 如果定义了deps1，则遍历deps1并将每个元素与来自deps2的等效元素进行比较
   //As this func is used only in this hook, we assume 2 deps always have same length.
+  // 因为这个func只在这个钩子中使用，所以我们假设2个deps的长度总是相同的。
   return deps1
     ? deps1
         .map((_ele, idx) => (!Object.is(deps1[idx], deps2?.[idx]) ? idx : -1))
@@ -21,10 +24,13 @@ const diffTwoDeps = (deps1?: DependencyList, deps2?: DependencyList) => {
 };
 
 const useTrackedEffect = (effect: Effect, deps?: DependencyList) => {
+  // 记录上次依赖
   const previousDepsRef = useRef<DependencyList>();
 
   useEffect(() => {
+    // 判断依赖的 changes
     const changes = diffTwoDeps(previousDepsRef.current, deps);
+    // 上次依赖
     const previousDeps = previousDepsRef.current;
     previousDepsRef.current = deps;
     return effect(changes, previousDeps, deps);
--- packages/hooks/src/useWhyDidYouUpdate/index.ts
+++ packages/hooks/src/useWhyDidYouUpdate/index.ts
@@ -7,10 +7,13 @@ export default function useWhyDidYouUpdate(componentName: string, props: IProps)
 
   useEffect(() => {
     if (prevProps.current) {
+      // 获取到所有的 keys
       const allKeys = Object.keys({ ...prevProps.current, ...props });
       const changedProps: IProps = {};
 
+      // 看哪些 key 进行了更新
       allKeys.forEach((key) => {
+        // 通过 Object.is 判断是否进行更新
         if (!Object.is(prevProps.current[key], props[key])) {
           changedProps[key] = {
             from: prevProps.current[key],
@@ -19,11 +22,13 @@ export default function useWhyDidYouUpdate(componentName: string, props: IProps)
         }
       });
 
+      // 有 diff，则输出
       if (Object.keys(changedProps).length) {
         console.log('[why-did-you-update]', componentName, changedProps);
       }
     }
 
+    // 记录上一次的值
     prevProps.current = props;
   });
 }
