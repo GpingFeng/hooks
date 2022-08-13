5e61de172afa6d109a4d012e91418f8f5ce3c6d3
Gopal <1172597655@qq.com>
2022-08-13 19:58:50 +0800

    docs: useExternal

--- packages/hooks/src/useExternal/index.ts
+++ packages/hooks/src/useExternal/index.ts
@@ -27,7 +27,7 @@ const loadScript = (path: string, props = {}): loadResult => {
     const newScript = document.createElement('script');
     // 设置 src
     newScript.src = path;
-    // 设置 props 值
+    // 设置属性值
     Object.keys(props).forEach((key) => {
       newScript[key] = props[key];
     });
@@ -53,12 +53,13 @@ const loadScript = (path: string, props = {}): loadResult => {
 // 加载 CSS
 const loadCss = (path: string, props = {}): loadResult => {
   const css = document.querySelector(`link[href="${path}"]`);
+  // 没有，则创建
   if (!css) {
     const newCss = document.createElement('link');
 
     newCss.rel = 'stylesheet';
     newCss.href = path;
-    // 设置 props
+    // 设置相应的属性
     Object.keys(props).forEach((key) => {
       newCss[key] = props[key];
     });
@@ -70,15 +71,19 @@ const loadCss = (path: string, props = {}): loadResult => {
       newCss.rel = 'preload';
       newCss.as = 'style';
     }
+    // 正在加载中
     newCss.setAttribute('data-status', 'loading');
+    // 在 head 标签中插入
     document.head.appendChild(newCss);
 
     return {
+      // 返回
       ref: newCss,
       status: 'loading',
     };
   }
 
+  // 有则直接返回，并取 data-status 中的值
   return {
     ref: css,
     status: (css.getAttribute('data-status') as Status) || 'ready',
@@ -92,9 +97,11 @@ const useExternal = (path?: string, options?: Options) => {
 
   useEffect(() => {
     if (!path) {
+      // 未设置
       setStatus('unset');
       return;
     }
+    // 处理 |个
     const pathname = path.replace(/[|#].*$/, '');
     // 判断是 CSS 类型
     if (options?.type === 'css' || (!options?.type && /(^css!|\.css$)/.test(pathname))) {
@@ -126,6 +133,7 @@ const useExternal = (path?: string, options?: Options) => {
     }
 
     const handler = (event: Event) => {
+      // 判断和设置加载状态
       const targetStatus = event.type === 'load' ? 'ready' : 'error';
       ref.current?.setAttribute('data-status', targetStatus);
       setStatus(targetStatus);
@@ -137,7 +145,7 @@ const useExternal = (path?: string, options?: Options) => {
     return () => {
       ref.current?.removeEventListener('load', handler);
       ref.current?.removeEventListener('error', handler);
-
+      // 卸载的时候 - 1
       EXTERNAL_USED_COUNT[path] -= 1;
 
       if (EXTERNAL_USED_COUNT[path] === 0) {
